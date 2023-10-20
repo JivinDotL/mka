86b943308c267eb05790388d729c37e4ad2aa765
Jivin.L <liu.jin@outlook.com>
2023-10-20 09:24:07 +0000

    add env.sh

---
+++ env.sh
@@ -0,0 +1,10 @@
+# gcc
+sudo apt install -y gcc libc6-dev
+# x11
+sudo apt install -y libx11-dev xorg-dev libxtst-dev
+# Clipboard
+sudo apt install -y xsel xclip
+# Bitmap
+sudo apt install -y libpng++-dev
+# Hook
+sudo apt install -y xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev libxkbcommon-x11-dev libxkbcommon-dev
